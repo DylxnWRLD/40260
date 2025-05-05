33b9ce05f032aa6c0d35713a39596fa0aea7f474
DylxnWRLD <dylaniscute.23@gmail.com>
2025-05-05 12:52:22 -0600

    08-fieldset

--- html00/formulario.js
+++ html00/formulario.js
@@ -3,10 +3,10 @@ console.log("Inicio")
 const button = document.getElementById("Enviar")
 button.addEventListener("click",
     () => {
-        console.log()
+        console.log("Los parámetros: ")
         let user=document.getElementById("user")
         let password=document.getElementById("password")
         console.log(password.value)
-        console.log(user)
+        console.log(user.value)
     }
  )
